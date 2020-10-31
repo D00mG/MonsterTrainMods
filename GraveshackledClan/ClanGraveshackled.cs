86ce0ff13c32779fe7b03c618f54b9a5a3c3b130
D00mG <73752520+D00mG@users.noreply.github.com>
2020-10-31 13:43:13 -0300

    Add files via upload

---
+++ GraveshackledClan/ClanGraveshackled.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Trainworks.Builders;
+using Trainworks.Constants;
+using Trainworks.Managers;
+
+namespace GraveshackledClan_Main
+{
+    class ClanGraveshackled
+    {
+        public static readonly string ID = "GraveshackledClanDefine_ID";
+
+        public static void Make()
+        {
+            new ClassDataBuilder
+            {
+                ClassID = ID,
+                Name = "Graveshackled Clan",
+                Description = "Raise the restless dead of the Graveshackled Clan. Draw sustenance from your enemy's forces and defeat them with afflictions and trickery.",
+                SubclassDescription = "Ally with sturdy Revenants and etherial Phantasms of the Graveshackled Clan.",
+                CardStyle = ClassCardStyle.Stygian,
+                IconAssetPaths = new List<string>
+                    {
+                        "assets/ClanArt/img_clan_graveshackled_small.png",
+                        "assets/ClanArt/img_clan_graveshackled_medium.png",
+                        "assets/ClanArt/img_clan_graveshackled_large.png",
+                        "assets/ClanArt/img_clan_graveshackled_silhouette.png"
+                    },
+                UiColor = new UnityEngine.Color(0.1f, 0.8f, 0.8f, 1f),
+                UiColorDark = new UnityEngine.Color(0.05f, 0.5f, 0.5f, 1f),
+            }.BuildAndRegister();
+        }
+    }
+}
---
+++ GraveshackledClan/GraveshackledClan_Main.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Trainworks.Interfaces;
+using Trainworks.Managers;
+using System;
+
+namespace GraveshackledClan_Main
+{
+    [BepInPlugin("doomg.mod.graveshackledclan", "Graveshackled Clan", "1.0.0")]
+    [BepInDependency("tools.modding.trainworks")]
+    public class GraveshackledClan_Main : BaseUnityPlugin, IInitializable
+    {
+        public void Initialize()
+        {
+            ClanGraveshackled.Make();
+            SubtypeGraveshackledRevenant.RegisterSubtypes();
+            RelicGraveshackledDisplacementLantern.Make();
+            UnitGraveshackledChampion_Inevitable.Make();
+            SpellGraveshackledBloodHarvest.Make();
+            UnitGraveshackledCharnelSlab.Make();
+            RewardNodeGraveshackled.Make();
+        }
+        void Awake()
+        {
+            var harmony = new Harmony("doomg.mod.graveshackledclan");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(SaveManager), "SetupRun")]
+        class AddToStartingDeck
+        {
+            static void Postfix(ref SaveManager __instance)
+            {
+                __instance.AddCardToDeck(CustomCardManager.GetCardDataByID("SpellGraveshackledBloodHarvest_ID"));
+                __instance.AddCardToDeck(CustomCardManager.GetCardDataByID("CardGraveshackledCharnelSlab_ID"));
+            }
+        }
+
+        //[HarmonyPatch(typeof(SaveManager), "SetupRun")]
+        //class AddRelicAtStartOfRunPatch
+        //{
+        //    static void Postfix(ref SaveManager __instance)
+        //    {
+        //        __instance.AddRelic(CustomCollectableRelicManager.GetRelicDataByID("RelicGraveshackledDisplacementLantern_ID"));
+        //    }
+        //}
+    }
+}
